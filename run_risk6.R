1edc74dab660b7ca2915ab0f874a6416f3c1be0b
Hanif Djunaedy <hanif.djunaedy@gmail.com>
2025-02-17 17:41:17 +0700

    Edited run_risk6.R and finished testing

--- run_risk6.R
+++ run_risk6.R
@@ -2,6 +2,9 @@ args <- commandArgs(trailingOnly = TRUE)
 
 csv_file <- args[1]
 
-pcr-data <- read.csv(csv_file, header = TRUE, row.names = 1
+pcrData <- read.csv(csv_file, header = TRUE, row.names = 1)
 
-print(pcr-data))
+source("risk6.R")
+risk6.results <- risk6(pcrData)
+
+write.csv(risk6.results, row.names = FALSE)
