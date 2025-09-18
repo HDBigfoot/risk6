309c9bad2f75453f4e90e2466c870c06cb5d24cc
Hanif Djunaedy <hanif.djunaedy@gmail.com>
2025-09-18 14:27:19 +0700

    added alternative run_risk6

---
+++ run_risk6_alt.R
@@ -0,0 +1,10 @@
+args <- commandArgs(trailingOnly = TRUE)
+
+csv_file <- args[1]
+
+pcrData <- read.csv(csv_file, header = TRUE, row.names = 1, check.names = FALSE)
+
+source("risk6.R")
+risk6.results <- risk6(pcrData)
+
+write.csv(risk6.results, row.names = FALSE)
